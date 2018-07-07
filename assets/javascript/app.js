71f8ec3b85bf4a6ae12b79a5405619dd65d45271
Kayle7777 <jessewebb2@gmail.com>
2018-07-07 14:24:05 -0400

    SOME STUFF

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,17 @@
 //NYT api key = 5fb1a34287ef406ba23992f37d8f0f60
-
 let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "5fb1a34287ef406ba23992f37d8f0f60",
-  'hl': "true"
-});
-
+function clearSearch() {
+  url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "5fb1a34287ef406ba23992f37d8f0f60",
+    'hl': "true"
+  });
+}
 function checkInputs() {
   if ($("#searchInput").val() != "") {
     url += `&q=${$("#searchInput").val().trim()}`
   } else {
-    url += `&q=Jonathan Coulton`
+    url += `&q=Jonathan%20Coulton`
   }
   if ($("#startDate").val() != "") {
     url += `&begin_date=${$("#startDate").val().trim()}`
@@ -21,15 +22,28 @@ function checkInputs() {
 }
 
 $("#searchButton").on("click", () => {
-  checkInputs()
-  console.log(url)
+  $("#newsDiv").empty();
+  clearSearch();
+  checkInputs();
+  console.log(url);
   $.ajax({
     url: url,
     method: 'GET',
   }).done(
-  function(response) {
-    console.log(response);
-    // PUT ALL CODE TO PUT STUFF FROM THE RESPONSE ON THE PAGE HERE
+  function(ajaxresponse) {
+    console.log(ajaxresponse);
+     ajaxresponse.response.docs.map((object, index) => {
+       let div = $("<div class='row'>");
+       let button = $("<button>");
+       let anchor = $("<a>");
+       button.attr("class", "col-md-12 button button-light");
+       anchor.attr("href", object.web_url);
+       anchor.attr("target", "_blank");
+       button.text(JSON.stringify(object.headline.main));
+       anchor.append(button);
+       div.append(anchor);
+       $("#newsDiv").append(div)
+     })
 
   }).fail(
     function(err) {
